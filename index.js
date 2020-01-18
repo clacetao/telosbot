fcf84c8edccb7802f006e3f42d4dd36c6846814e
clacetao <60002955+clacetao@users.noreply.github.com>
2020-01-18 07:57:12 -0300

    qualquer coisa

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-const ytdl = require('ytdl-core')
+const client = new Discord.Client();
+const ytdl = require('ytdl-core');
 const streamOptions = {seek: 0, volume: 0.8};
+const queue = new Map();
 
-bot.login('NjY3MDIxNTI0NDUyMjQ1NTA1.Xh8_0w.YFtOCcnv2SEgse8QQVBEMz86jJw');
+client.once('ready', () => {
+	console.log('Ready!');
+});
 
-bot.on('msg', msg => {
+client.once('reconnecting', () => {
+	console.log('Reconnecting!');
+});
+
+client.once('disconnect', () => {
+    console.log('Disconnect!');
+    
+});
+
+client.login('NjY3MDIxNTI0NDUyMjQ1NTA1.Xh8_0w.YFtOCcnv2SEgse8QQVBEMz86jJw');
+
+client.on('message', message => {
 let responseObject = {
+    
 
     "Telos" : "Telos é o cara",
+    "telos" : "Telos é o cara",
     "Quem é você?" : "Quem é você queridão????????",
     "quem é você?" : "Quem é você queridão????????",
     "Telos é ruim?" : "Telos é ruim? Onde ja se viu uma pessoa com esse controle de códigos, Telos deixou a programação em outro nível, nós seremos abençoados em termos progamadores com esse nível, você pode ficar com o pedro, eu prefiro o Telos.",
@@ -18,60 +34,109 @@ let responseObject = {
     "Você joga frifas" : "Sai daí seu gemado no frifas",
 };
 
-if(responseObject[msg.content]){
-    msg.channel.send(responseObject[msg.content]);
+if(responseObject[message.content]){
+    message.channel.send(responseObject[message.content]);
 }
 
-if (msg.content.startsWith("eu sou jogador de freefire?")){
+if (message.content.startsWith("eu sou jogador de freefire?")){
     randomNumber = Math.floor(Math.random() * (6 - 1) + 1);
     if(randomNumber == 2){
-        msg.reply("Não!");
+        message.reply("Não!");
     }
     else{
-        msg.reply("Sim!");
+        message.reply("Sim!");
     }
 }
 });
-bot.on('message', msg => {
-    if (msg.autor) {
-        return;
+client.on('message', async message => {
+    if (message.author.bot) return;
+    
+    const serverQueue = queue.get(message.guild.id);
+    
+    if (message.content.startsWith(`.toca`)) {
+ execute(message, serverQueue);
+ return;
+} else if (message.content.startsWith(`.pula`)) {
+ skip(message, serverQueue);
+ return;
+} else if (message.content.startsWith(`.para`)) {
+ stop(message, serverQueue);
+ return;
+
+};
+
+async function execute(message, serverQueue) {
+    const args = message.content.split(' ');
+    const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel) return message.channel.send('você precisa estar em um chat de voz');
+     const permissions =     voiceChannel.permissionsFor(message.client.user);
+    if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+     return message.channel.send('Preciso de permissão pra falar no seu canal');
     }
 
-    if(msg.content.indexOf("youtube") !== -1 && msg.content.toLowerCase().startsWith("?play")){
-        let Completemsg = msg.content.split(' ');
-        
-        let youtubeLink = Completemsg[1];
-        
-        
-        let VoiceChannel = msg.guild.channels.find(channel => channel.id === '666840371090948097');
+    const songInfo = await ytdl.getInfo(args[1]);
+const song = {
+ title: songInfo.title,
+ url: songInfo.video_url,
+};
+
+// Creating the contract for our queue
+const queueContruct = {
+    textChannel: message.channel,
+    voiceChannel: voiceChannel,
+    connection: null,
+    songs: [],
+    volume: 5,
+    playing: true,
+   };
+   // Setting the queue using our contract
+   queue.set(message.guild.id, queueContruct);
+   // Pushing the song to our songs array
+   queueContruct.songs.push(song);
+   
+   try {
+    // Here we try to join the voicechat and save our connection into our object.
+    var connection = await voiceChannel.join();
+    queueContruct.connection = connection;
+    // Calling the play function to start a song
+    play(message.guild, queueContruct.songs[0]);
+   } catch (err) {
+    // Printing the error message if the bot fails to join the voicechat
+    console.log(err);
+    queue.delete(message.guild.id);
+    return message.channel.send(err);
+   }
+
+   function play(guild, song) {
+    const serverQueue = queue.get(guild.id);
+    if (!song) {
+     serverQueue.voiceChannel.leave();
+     queue.delete(guild.id);
+     return;
     }
 
-                if(msg.content.indexOf("youtube") !== -1 && msg.content.toLowerCase().startsWith("?play")){
-                    let Completemsg = msg.content.split(' ');
-                    
-                    let youtubeLink = Completemsg[1];
-                }
-                    
-                    let VoiceChannel = msg.guild.channels.find(channel => channel.id === '666807105193508875');
-                    
-                    if(msg.content.indexOf("youtube") !== -1 && msg.content.toLowerCase().startsWith("?play")){
-                        let Completemsg = msg.content.split(' ');
-                        
-                        let youtubeLink = Completemsg[1];
-                    }   
-                            if(msg.content.indexOf("youtube") !== -1 && msg.content.toLowerCase().startsWith("?play")){
-                                let Completemsg = msg.content.split(' ');
-                                
-                                let youtubeLink = Completemsg[1];
-                            }
-                                    VoiceChannel.join()
-                                    .then(connection => {
-                                        const stream = ytdl(youtubeLink, {filter: 'audioonly'});
-
-                                        const DJ = connection.playStream(stream, streamOptions)
-                                        
-                                        DJ.on('end', end => {
-                                            VoiceChannel.leave();
-                                        });
-                                    })
+    const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+ .on('end', () => {
+  console.log('Terminou aeeeeee');
+  // Deletes the finished song from the queue
+  serverQueue.songs.shift();
+  // Calls the play function again with the next song
+  play(guild, serverQueue.songs[0]);
 })
+ .on('error', error => {
+  console.error(error);
+ });
+dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+
+function skip(message, serverQueue) {
+    if (!message.member.voiceChannel) return message.channel.send('Entra num chat de voz que eu quero ver');
+    if (!serverQueue) return message.channel.send('I rapaz, não consigo');
+    serverQueue.connection.dispatcher.end();
+   }
+
+   function stop(message, serverQueue) {
+    if (!message.member.voiceChannel) return message.channel.send('Entra num chat de voz que eu quero ver');
+    serverQueue.songs = [];
+    serverQueue.connection.dispatcher.end();
+   
+   }}}})
